121d18d56b1281c191fdedacb61c21148c46df72
Niwazou1 <yuunosuke_16n1101227@nnn.ed.jp>
2017-11-08 15:29:35 +0900

    pi

---
+++ Assets/scripts/RadiusCenter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class RadiusCenter : MonoBehaviour
+{
+    public Material radiusMaterial;
+    public Color color = Color.white;
+    public float radius = 10f;
+    public float radiusWidth = 2f;
+    public float power = 5f;
+    public float speed = 60f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        radiusMaterial.SetVector("_Center", transform.position);
+        radiusMaterial.SetColor("_RadiusColor", color);
+        radiusMaterial.SetFloat("_Radius", radius);
+        radiusMaterial.SetFloat("_RadiusWidth", radiusWidth);
+        radiusMaterial.SetFloat("_RadiusPower", power);
+        radiusMaterial.SetFloat("_RadiusSpeed", speed);
+    }
+}
